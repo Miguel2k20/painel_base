67aa7272e61d395c8040d8661ec8b5e5ba9ac72c
Miguel2k20 <joaomiguel2k20@gmail.com>
2025-11-01 11:13:56 -0300

    Add update user route and refactor user controller for improved functionality

--- src/Routes/UserRoutes.ts
+++ src/Routes/UserRoutes.ts
@@ -1,12 +1,13 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from "fastify";
 import { AuthMiddleware } from "../middleware/authMiddleware";
 import { userSchema } from "../schemas/userSchema";
-import { create, show } from "../controllers/userController";
+import { create, show, update } from "../controllers/userController";
 
 
 export async function UserRoutes(fastify:FastifyInstance, Options:FastifyPluginOptions) {
     fastify.addHook("preHandler", AuthMiddleware);
 
     fastify.post("/users", {schema: userSchema.create}, create);
+    fastify.put("/users/:id", update);
     fastify.get("/show", show);
 }
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -9,6 +9,10 @@ interface CreateUserBody {
     password: string;
 }
 
+interface requestParams {
+    id: string;
+}
+
 export async function create(request:FastifyRequest, reply:FastifyReply) {
 
     const { name, email, password } = request.body as CreateUserBody
@@ -63,4 +67,14 @@ export async function show(request:FastifyRequest, reply:FastifyReply) {
     }catch(err) {
         return reply.status(401).send({ message: "Token inválido ou expirado" });
     }
+}
+
+export async function update(request:FastifyRequest, reply:FastifyReply) {
+    const { id } = request.params as requestParams;
+    
+    reply.status(200).send({
+        statusCode: 200,
+        message: `Rota de update de usuario com id: ${id} em construção!`
+    });
+
 }
